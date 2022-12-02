67ed432c8be705da889c6785d0fb72356f2a645d
Jon Olav Kristoffersen <77264129+JonOlavK@users.noreply.github.com>
2022-12-02 09:45:16 +0100

    Day 2 Part 1

--- AdventOfCode2022/Day2.cs
+++ AdventOfCode2022/Day2.cs
@@ -8,8 +8,36 @@ public class Day2
         foreach (var input in inputs)
         {
             var characters = input.Split(' ');
+            var points = GetResult(characters[1], characters[0]);
+            points += GetNumberRepresentation(characters[1]);
+            result += points;
         }
 
         return result;
+    }
+
+    private static int GetResult(string player, string opponent)
+    {
+        var playerNumber = GetNumberRepresentation(player);
+        var opponentNumber = GetNumberRepresentation(opponent);
+        if (playerNumber == opponentNumber)
+            return 3;
+        if (playerNumber - opponentNumber == 1 || playerNumber - opponentNumber == -2)
+            return 6;
+        return 0;
+    }
+
+    private static int GetNumberRepresentation(string input)
+    {
+        return input switch
+        {
+            "A" => 1,
+            "X" => 1,
+            "B" => 2,
+            "Y" => 2,
+            "C" => 3,
+            "Z" => 3,
+            _ => throw new InvalidOperationException()
+        };
     }
 }
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,9 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Diagnostics;
 using AdventOfCode2022;
 
-Console.WriteLine("Hello, World!");
 var inputs = File.ReadAllLines("./Input/day2.txt");
 var result = Day2.Part1(inputs);
-Debugger.Break();
+Console.WriteLine(result);
