1e3c18a3efae19925db51870a6c997c0c4f9fb47
Mastereye7 <77264129+JonOlavK@users.noreply.github.com>
2022-12-02 07:58:50 +0100

    Starting

---
+++ AdventOfCode2022/Day2.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode2022;
+
+public class Day2
+{
+    public static int Part1(string[] inputs)
+    {
+        var result = 0;
+        foreach (var input in inputs)
+        {
+            var characters = input.Split(' ');
+        }
+
+        return result;
+    }
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using AdventOfCode2022;
+
 Console.WriteLine("Hello, World!");
+var inputs = File.ReadAllLines("./Input/day2.txt");
+var result = Day2.Part1(inputs);
+Debugger.Break();
